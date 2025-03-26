268b0afc57a08f6d17d970979f74a8a3d4f71318
CyberDream <gaetanosp@protonmail.com>
2025-03-26 23:29:12 +0100

    image test senza /

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -106,7 +106,7 @@ function HomePage() {
             <ServiceCard
               title="Corsi di ballo e attivita' fisica"
               description="Corsi di tango argentino e ginnastica soft. Lezioni per tutte le età e livelli di esperienza."
-              imageUrl="/assets/immaginetango.jpg"
+              imageUrl="assets/immaginetango.jpg"
               icon={<Music className="w-8 h-8 text-primary-600" />}
               link="/servizi/ballo"
             />
